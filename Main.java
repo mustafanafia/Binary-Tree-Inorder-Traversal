f33cf7e349ce51ed1f9ef2ac206c312dbebfd828
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-06-02 15:40:28 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        List<Integer> output_list = new ArrayList<>();
+
+        TreeNode current = root;
+
+
+        if(root == null){return output_list;}
+
+        while(current != null || !stack.isEmpty()){
+
+            while(current != null){
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            output_list.add(current.val);
+            current = current.right;
+        }
+
+        return output_list;
+
+    }
+}
